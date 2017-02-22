6c4d824b57525867abdded5eb97c905531793b10
Sarvaruis <krzwin13@gmail.com>
2017-02-23 00:07:54 +0100

    Podstawowe biblioteki oraz zalążek szkieletu projektu

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+
+namespace Politechnikon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameWindow Window = new GameWindow(600, 600);
+            Window.Run();
+        }
+    }
+}
