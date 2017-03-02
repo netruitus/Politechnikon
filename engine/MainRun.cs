008d7eee69b7e58d48b21bbfff1bc55a45f29ca7
Sarvaruis <krzwin13@gmail.com>
2017-03-02 09:15:16 +0100

    Update związany z Issue - stworzenie klasy silnikowej

---
+++ engine/MainRun.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Politechnikon.engine
+{
+    public class MainRun
+    {
+        private static Engine Game;
+        public static void Main(string[] args)
+        {
+            Game = new Engine(800, 500);
+            Game.Run();
+        }
+    }
+}
