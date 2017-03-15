b958da84ed662e0808f8f6356d09f42f17e8aa06
Sarvaruis <krzwin13@gmail.com>
2017-03-15 10:17:24 +0100

    klasy obiektów oraz logiki gry

---
+++ game/Logic.cs
@@ -0,0 +1,29 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Politechnikon.game
+{
+    public class Logic
+    {
+
+
+        public void render(FrameEventArgs e)
+        {
+
+        }
+
+        public void update()
+        {
+
+        }
+
+        public void getInput()
+        {
+
+        }
+    }
+}
