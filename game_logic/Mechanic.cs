f69c280cdb11e08086c37ca8778785a0e431b483
Sarvaruis <krzwin13@gmail.com>
2017-04-17 14:06:45 +0200

    Refaktoryzacja i restrukturyzacja klas Part.1

--- game_logic/Mechanic.cs
+++ game_logic/Mechanic.cs
@@ -4,19 +4,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using Politechnikon.engine;
 
 namespace Politechnikon.game_logic
 {
     public class Mechanic
     {
-        public Mechanic()
+        private List<InitializedObjectTexture> ObjectList;
+        private bool IsGameLoaded = false, IsGameStarted = false;
+       
+        public Mechanic(List<InitializedObjectTexture> objectList)
         {
-
+            this.ObjectList = objectList;
         }
 
-        public void LoadObjects()
+        public void InitObjects()
         {
-
+            //inicjalizacja obiektów - ładowanie poszczególnych tekstur wyświetlanych do listy obiektów do wyrenderowania przez silnik
+            InitializedObjectTexture TempObject = new InitializedObjectTexture(500, 0, 300, 500, "GUI\\ekran_startowy.png", Color.White);
+            ObjectList.Add(TempObject);
         }
 
         public void GetInput()
@@ -24,13 +31,6 @@ namespace Politechnikon.game_logic
 
         }
 
-
-
-
-        public void render(FrameEventArgs e)
-        {
-
-        }
 
         public void update()
         {
