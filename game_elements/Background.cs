bb707f68845c21203d6c91f25b6aedbeb538c3d5
Sarvaruis <krzwin13@gmail.com>
2017-05-24 04:26:46 +0200

    update klasy background

--- game_elements/Background.cs
+++ game_elements/Background.cs
@@ -1,12 +1,55 @@
-﻿using System;
+﻿using Politechnikon.engine;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Politechnikon.objects
+namespace Politechnikon.game_elements
 {
-    class Background
+    public enum BackgroundType
     {
+        Board,
+        GUI
+    }
+
+    public class Background : ObjectAbstrakt
+    {
+        private BackgroundType Type;
+
+        public Background(int x, int y, int id, BackgroundType type)
+        {
+            this.Id = id;
+            this.X = x;
+            this.Y = y;
+            this.Type = type;
+
+            if (this.Type == BackgroundType.Board)
+            {
+                LoadBoardVariables(this.Id);
+            }
+            else if (this.Type == BackgroundType.GUI)
+            {
+                LoadGUIVariables(this.Id);
+            }
+        }
+
+        private void LoadBoardVariables(int id)
+        {
+            XMLParser parser = new XMLParser("Backgrounds.xml");
+            ParseName(parser);
+            ParsePath(parser);
+            ParseSizeX(parser);
+            ParseSizeY(parser);
+        }
+
+        private void LoadGUIVariables(int id)
+        {
+            XMLParser parser = new XMLParser("GUIs.xml");
+            ParseName(parser);
+            ParsePath(parser);
+            ParseSizeX(parser);
+            ParseSizeY(parser);
+        }
     }
 }
