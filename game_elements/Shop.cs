c3a7218c4104b96b0fcb798a0923c70f5ed2f474
Sarvaruis <krzwin13@gmail.com>
2017-05-24 04:38:32 +0200

    update klasy Shop

--- game_elements/Shop.cs
+++ game_elements/Shop.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using Politechnikon.engine;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Politechnikon.objects
+namespace Politechnikon.game_elements
 {
-    class Shop
+    public class Shop : ObjectAbstrakt
     {
+        public Shop(int x, int y, int id)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Id = id;
+
+            LoadShopVariables(this.Id);
+        }
+
+        private void LoadShopVariables(int id)
+        {
+            XMLParser parser = new XMLParser("Shops.xml");
+            ParseName(parser);
+            ParsePath(parser);
+            ParseDescription(parser);
+            ParseSizeX(parser);
+            ParseSizeY(parser);
+        }
     }
 }
